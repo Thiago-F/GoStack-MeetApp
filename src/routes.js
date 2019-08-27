2b5bcdbe30e2352d269e52343e91e81a1da911d1
Thiago Fialho <thiago_fialho150@hotmail.com>
2019-08-27 20:55:24 -0300

    user update

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,20 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 // cadastro de usuario
 routes.post('/users', UserController.store);
-routes.post('/sessions', SessionController.store);
 
 // iniciar sessão de usuario
+routes.post('/sessions', SessionController.store);
+
+// middleware de autenticação
+routes.use(authMiddleware);
 
 // update de usuario
+routes.put('/users', UserController.update);
 
 export default routes;
